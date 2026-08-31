ef9576a85e2f7517e04c7382786f4d9a3d05bb28
Xiao_Yu233 <37735771+XiaoYuOvO@users.noreply.github.com>
2026-08-31 21:32:06 +0800

    chore(network): update UA

--- CourseRush.Core/Network/WebClient.cs
+++ CourseRush.Core/Network/WebClient.cs
@@ -114,7 +114,7 @@ public class WebClient
         var httpRequestMessage = new HttpRequestMessage();
         httpRequestMessage.RequestUri = uri;
         httpRequestMessage.Headers.Host = uri.Host;
-        httpRequestMessage.Headers.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0");
+        httpRequestMessage.Headers.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:151.0) Gecko/20100101 Firefox/151.0");
         httpRequestMessage.Headers.Add("Accept", accept.ToHeaderString());
         httpRequestMessage.Headers.Add("Accept-Language", "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2");
         httpRequestMessage.Headers.Add("Accept-Encoding", encoding);
